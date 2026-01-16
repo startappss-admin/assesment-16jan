9ba6dace921f724d5513b1f7d402b242e4f350a8
rajeev-raghuwanshi <rajeevraghuwanshi450@gmail.com>
2026-01-16 17:08:06 +0530

    completed both the assements

--- react-chessboard/src/index.js
+++ react-chessboard/src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Chessboard from './components/Chessboard';
 
 const root = ReactDOM.createRoot(document.getElementById('root1'));
 root.render(
   <React.StrictMode>
-    <>
-    <h1>hellow world how are you </h1>
-    </>
+    <Chessboard></Chessboard>
     {/* <App /> */}
   </React.StrictMode>
 );
